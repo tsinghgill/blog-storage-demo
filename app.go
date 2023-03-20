4938c6e88c372df9ebbafea5238eec60e94c13ba
Meroxa <meroxa@Meroxas-MacBook-Pro.local>
2023-03-20 16:42:22 -0400

    Post to webhook site

--- app.go
+++ app.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	// Dependencies of the example data app
-
-	// Dependencies of Turbine
-
+	"bytes"
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	"net/http"
+	"time"
 
+	// Dependencies of Turbine
 	"github.com/meroxa/turbine-go"
 	"github.com/meroxa/turbine-go/runner"
 )
@@ -51,14 +52,10 @@ func (a App) Run(v turbine.Turbine) error {
 type Anonymize struct{}
 
 func (f Anonymize) Process(stream []turbine.Record) []turbine.Record {
-	for i, record := range stream {
-		// Convert the record struct to a JSON object
-		recordJSON, err := json.MarshalIndent(record, "", "  ")
-		if err != nil {
-			fmt.Printf("Error converting record %d to JSON: %v\n", i+1, err)
-			continue
-		}
+	webhookURL := "https://webhook.site/ac38ce4e-c50a-4a56-b098-8b038890d94f"
+	baseURL := "https://meroxas3bucket.s3.us-east-2.amazonaws.com/"
 
+	for i, record := range stream {
 		// Decode the base64-encoded payload
 		var key struct {
 			Schema  struct{} `json:"schema"`
@@ -74,11 +71,53 @@ func (f Anonymize) Process(stream []turbine.Record) []turbine.Record {
 			continue
 		}
 
-		// Log out the record and decoded payload
-		fmt.Printf("Record %d:\n", i+1)
-		fmt.Println(string(recordJSON))
+		// Log out the decoded payload
 		fmt.Printf("Decoded Payload for Record %d:\n", i+1)
 		fmt.Println(string(payload))
+
+		// Construct the full URL by concatenating the baseURL and the decoded payload
+		fullURL := baseURL + string(payload)
+
+		// Create a map with a single key-value pair, where the key is "url" and the value is the fullURL
+		postData := map[string]string{"url": fullURL}
+
+		// Marshal the postData map into JSON data
+		jsonData, err := json.Marshal(postData)
+		if err != nil {
+			fmt.Printf("Error marshalling JSON data in record %d: %v\n", i+1, err)
+			continue
+		}
+
+		// Create a new POST request with the webhookURL and jsonData as the request body
+		req, err := http.NewRequest("POST", webhookURL, bytes.NewBuffer(jsonData))
+		if err != nil {
+			fmt.Printf("Error creating request for record %d: %v\n", i+1, err)
+			continue
+		}
+		// Set the request's Content-Type header to "application/json"
+		req.Header.Set("Content-Type", "application/json")
+
+		// Create an HTTP client with a 10-second timeout
+		client := &http.Client{Timeout: time.Second * 10}
+
+		// Send the POST request using the client
+		resp, err := client.Do(req)
+		if err != nil {
+			fmt.Printf("Error sending request for record %d: %v\n", i+1, err)
+			continue
+		}
+		// Close the response body to prevent resource leaks
+		defer resp.Body.Close()
+
+		// Check if the response status code is OK (200)
+		if resp.StatusCode != http.StatusOK {
+			fmt.Printf("Error: non-OK HTTP status for record %d: %d\n", i+1, resp.StatusCode)
+			continue
+		}
+
+		// Print a message indicating that the record was successfully posted to webhook.site
+		fmt.Printf("Successfully posted Record %d to webhook.site\n", i+1)
 	}
+
 	return stream
 }
