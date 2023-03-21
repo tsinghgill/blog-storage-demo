8ea8d486d701872796cae636a17aab2c8b178123
Meroxa <meroxa@Meroxas-MacBook-Pro.local>
2023-03-21 15:03:56 -0400

    test

--- app.go
+++ app.go
@@ -38,7 +38,7 @@ func (a App) Run(v turbine.Turbine) error {
 		return err
 	}
 
-	res := v.Process(rr, Anonymize{})
+	res := v.Process(rr, Transform{})
 
 	dest, err := v.Resources("pg_db")
 	if err != nil {
@@ -53,129 +53,140 @@ func (a App) Run(v turbine.Turbine) error {
 	return nil
 }
 
-type Anonymize struct{}
+type Transform struct{}
 
-func (f Anonymize) Process(stream []turbine.Record) []turbine.Record {
+func (f Transform) Process(stream []turbine.Record) []turbine.Record {
 	var outputStream []turbine.Record
 
 	webhookURL := "https://webhook.site/ac38ce4e-c50a-4a56-b098-8b038890d94f"
 	baseURL := "https://meroxas3bucket.s3.us-east-2.amazonaws.com/"
 
 	for i, record := range stream {
-		// Decode the base64-encoded payload
-		var key struct {
-			Schema  struct{} `json:"schema"`
-			Payload string   `json:"payload"`
-		}
-		if err := json.Unmarshal([]byte(record.Key), &key); err != nil {
-			fmt.Printf("Error Unmarshalling key in record %d: %v\n", i+1, err)
-			continue
-		}
-
-		keyPayload, err := base64.StdEncoding.DecodeString(key.Payload)
-		// Log out the decoded keyPayload
-		fmt.Printf("Decoded keyPayload for Record %s:", string(keyPayload))
+		keyPayload, err := decodeKeyPayload([]byte(record.Key))
 		if err != nil {
-			fmt.Printf("Error decoding key.Payload in record%d: %v\n", i+1, err)
+			fmt.Printf("Error decoding key.Payload in record %d: %v\n", i+1, err)
+			continue
+		}
+		fmt.Printf("Decoded keyPayload for Record %s:", keyPayload)
+
+		recordPayloadUnmarshalled, err := unmarshalRecordPayload(record.Payload)
+		if err != nil {
+			fmt.Printf("Error Unmarshalling recordPayloadUnmarshalled in record %d: %v\n", i+1, err)
 			continue
 		}
 
-		var recordPayloadUnmarshalled map[string]interface{}
+		fullURL := baseURL + keyPayload
+		operation := getOperation(recordPayloadUnmarshalled["payload"].(map[string]interface{})["op"].(string))
 
-		if err := json.Unmarshal(record.Payload, &recordPayloadUnmarshalled); err != nil {
-			fmt.Printf("Error Unmarshalling recordPayloadUnmarshalled in record%d: %v\n", i+1, err)
-			continue
-		}
-		// fmt.Printf("recordPayloadUnmarshalled >>> %+v\n", recordPayloadUnmarshalled)
-		fmt.Printf("recordPayloadUnmarshalled.op >>> %s\n", recordPayloadUnmarshalled["payload"].(map[string]interface{})["op"].(string))
-
-		// Construct the full URL by concatenating the baseURL and the decoded payload
-		fullURL := baseURL + string(keyPayload)
-		operation := recordPayloadUnmarshalled["payload"].(map[string]interface{})["op"].(string)
-
-		switch operation {
-		case "c":
-			operation = "CREATE"
-		case "u":
-			operation = "UPDATE"
-		case "d":
-			operation = "DELETE"
-		case "r":
-			operation = "SNAPSHOT"
-		default:
-			operation = "UNKNOWN"
-		}
-
-		// Create a map with a single key-value pair, where the key is "url" and the value is the fullURL
 		postData := map[string]string{"url": fullURL, "operation": operation}
-
-		// Marshal the postData map into JSON data
 		jsonData, err := json.Marshal(postData)
 		if err != nil {
 			fmt.Printf("Error marshalling JSON data in record %d: %v\n", i+1, err)
 			continue
 		}
 
-		// Create a new POST request with the webhookURL and jsonData as the request body
-		req, err := http.NewRequest("POST", webhookURL, bytes.NewBuffer(jsonData))
-		if err != nil {
-			fmt.Printf("Error creating request for record %d: %v\n", i+1, err)
-			continue
-		}
-		// Set the request's Content-Type header to "application/json"
-		req.Header.Set("Content-Type", "application/json")
-
-		// Create an HTTP client with a 10-second timeout
-		client := &http.Client{Timeout: time.Second * 3}
-
-		// Send the POST request using the client
-		fmt.Printf("Sending req to webhook.site: %v\n", req)
-		resp, err := client.Do(req)
+		err = sendPOSTRequest(webhookURL, jsonData)
 		if err != nil {
 			fmt.Printf("Error sending request for record %d: %v\n", i+1, err)
 			continue
 		}
-		// Close the response body to prevent resource leaks
-		defer resp.Body.Close()
 
-		// Check if the response status code is OK (200)
-		if resp.StatusCode != http.StatusOK {
-			fmt.Printf("Error: non-OK HTTP status for record %d: %d\n", i+1, resp.StatusCode)
-			continue
-		}
-
-		// Print a message indicating that the record was successfully posted to webhook.site
 		fmt.Printf("Successfully posted Record %d to webhook.site\n", i+1)
 
-		// Create the desired schema and payload structures
-		structuredPayload := kcschema.StructuredPayload{
-			"url": kcschema.Field{
-				Type:  kcschema.StringField,
-				Value: fullURL,
-			},
-			"operation": kcschema.Field{
-				Type:  kcschema.StringField,
-				Value: operation,
-			},
-		}
-
-		// Marshal the StructuredPayload into a JSON array of bytes using AsKCSchemaJSON
-		jsonValue, err := structuredPayload.AsKCSchemaJSON("resource.public.collection_name.Value")
+		newRecord, err := createNewRecord([]byte(record.Key), record.Timestamp, fullURL, operation)
 		if err != nil {
-			fmt.Printf("Error marshalling new value in record: %v\n", err)
+			fmt.Printf("Error creating new record for record %d: %v\n", i+1, err)
 			continue
 		}
 
-		// Create a new turbine.Record object with the desired structure
-		newRecord := turbine.Record{
-			Key:       record.Key,
-			Payload:   jsonValue,
-			Timestamp: record.Timestamp,
-		}
-
-		// Add the new record to the outputStream
 		outputStream = append(outputStream, newRecord)
 	}
 
 	return outputStream
+}
+
+func decodeKeyPayload(recordKey []byte) (string, error) {
+	var key struct {
+		Schema  struct{} `json:"schema"`
+		Payload string   `json:"payload"`
+	}
+	if err := json.Unmarshal(recordKey, &key); err != nil {
+		return "", err
+	}
+	keyPayload, err := base64.StdEncoding.DecodeString(key.Payload)
+	if err != nil {
+		return "", err
+	}
+	return string(keyPayload), nil
+}
+
+func unmarshalRecordPayload(recordPayload []byte) (map[string]interface{}, error) {
+	var recordPayloadUnmarshalled map[string]interface{}
+	err := json.Unmarshal(recordPayload, &recordPayloadUnmarshalled)
+	if err != nil {
+		return nil, err
+	}
+	return recordPayloadUnmarshalled, nil
+}
+
+func getOperation(operation string) string {
+	switch operation {
+	case "c":
+		return "CREATE"
+	case "u":
+		return "UPDATE"
+	case "d":
+		return "DELETE"
+	case "r":
+		return "SNAPSHOT"
+	default:
+		return "UNKNOWN"
+	}
+}
+
+func sendPOSTRequest(webhookURL string, jsonData []byte) error {
+	req, err := http.NewRequest("POST", webhookURL, bytes.NewBuffer(jsonData))
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{Timeout: time.Second * 3}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("non-OK HTTP status: %d", resp.StatusCode)
+	}
+
+	return nil
+}
+
+func createNewRecord(recordKey []byte, recordTimestamp time.Time, fullURL string, operation string) (turbine.Record, error) {
+	structuredPayload := kcschema.StructuredPayload{
+		"url": kcschema.Field{
+			Type:  kcschema.StringField,
+			Value: fullURL,
+		},
+		"operation": kcschema.Field{
+			Type:  kcschema.StringField,
+			Value: operation,
+		},
+	}
+
+	jsonValue, err := structuredPayload.AsKCSchemaJSON("resource.public.collection_name.Value")
+	if err != nil {
+		return turbine.Record{}, err
+	}
+
+	newRecord := turbine.Record{
+		Key:       string(recordKey),
+		Payload:   jsonValue,
+		Timestamp: recordTimestamp,
+	}
+
+	return newRecord, nil
 }
